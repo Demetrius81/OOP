2563e9387ca98c61c01078e4679ac217859354b9
Dmitri <Shizimaniak@gmail.com>
2022-03-22 12:04:03 +0300

    Начал работу над ДЗ

---
+++ Lesson2/Lesson2/Task1/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2.Task1
+{
+    //  Создать класс счет в банке с закрытыми полями:
+    //      номер счета,
+    //      баланс,
+    //      тип банковского счета (использовать перечислимый тип).
+    //  Предусмотреть методы для доступа к данным – заполнения и чтения.
+    //  Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
+
+
+    enum BankAccountType
+    {
+        current   = 1,
+        credit    = 2,
+        deposit   = 3,
+        budgetary = 4
+    }
+
+    internal class BankAccount
+    {
+        private int _accountId;
+
+        private decimal _currentBalance;
+
+        private BankAccountType _bankAccountType;
+    }
+}
