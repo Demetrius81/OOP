53820799872b461bc45ad6f3be1a1aefde0ed0b7
Dmitri <Shizimaniak@gmail.com>
2022-03-25 16:22:51 +0300

    start

--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -1,19 +1,12 @@
-﻿
-namespace Lesson2
+﻿using System;
+
+namespace Lesson3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Task1.Demo.Task1Demo();
-
-            Task2.Demo.Task2Demo();
-
-            Task3.Demo.Task3Demo();
-
-            Task4.Demo.Task4Demo();
-
-            Task5.Demo.Task5Demo();
+            Console.WriteLine("Hello World!");
         }
     }
 }
